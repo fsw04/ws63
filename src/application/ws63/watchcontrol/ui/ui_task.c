d879056c8f215578c9877c0c07ab435b87e270b4
fsw04 <fsw04@users.noreply.github.com>
2026-05-28 18:18:51 +0000

    feat: 设计WatchControl UI界面
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/application/ws63/watchcontrol/ui/ui_task.c
+++ src/application/ws63/watchcontrol/ui/ui_task.c
@@ -13,8 +13,6 @@
 #define UI_TASK_PRIORITY      20
 #define UI_TASK_NAME          "UITask"
 
-#define COLOR_BG              0x0A1E1A
-
 #define LOG_MAX_ENTRIES       50
 
 typedef enum {
@@ -23,16 +21,8 @@ typedef enum {
     LOG_LEVEL_ERROR,
 } log_level_t;
 
-typedef struct {
-    char timestamp[16];
-    log_level_t level;
-    char type[16];
-    char message[64];
-} log_entry_t;
-
 static lv_obj_t *g_tabview = NULL;
 static lv_obj_t *g_log_list = NULL;
-static log_entry_t g_log_entries[LOG_MAX_ENTRIES];
 static int g_log_count = 0;
 
 static const lv_color_t log_level_colors[] = {
@@ -71,9 +61,9 @@ static void ui_add_log_entry(const char *type, log_level_t level, const char *me
 
     lv_obj_t *time_label = lv_label_create(row);
     lv_label_set_text_fmt(time_label, "%02d:%02d:%02d",
-        (int)(osal_get_sys_time() / 3600000) % 24,
-        (int)(osal_get_sys_time() / 60000) % 60,
-        (int)(osal_get_sys_time() / 1000) % 60);
+        (int)(osal_get_jiffies() / 3600000) % 24,
+        (int)(osal_get_jiffies() / 60000) % 60,
+        (int)(osal_get_jiffies() / 1000) % 60);
     lv_obj_set_style_text_font(time_label, &lv_font_montserrat_10, 0);
     lv_obj_set_style_text_color(time_label, lv_color_hex(0x888888), 0);
 
@@ -102,7 +92,7 @@ static void create_log_page(lv_obj_t *parent)
     lv_obj_set_flex_flow(parent, LV_FLEX_FLOW_COLUMN);
     lv_obj_set_flex_align(parent, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START);
     lv_obj_set_style_pad_all(parent, 8, 0);
-    lv_obj_set_style_bg_color(parent, lv_color_hex(COLOR_BG), 0);
+    lv_obj_set_style_bg_color(parent, COLOR_BG, 0);
 
     lv_obj_t *title = lv_label_create(parent);
     lv_label_set_text(title, "系统日志");
@@ -133,7 +123,7 @@ static void *ui_main_task(const char *arg)
     ui_style_init();
 
     lv_obj_t *scr = lv_screen_active();
-    lv_obj_set_style_bg_color(scr, lv_color_hex(COLOR_BG), 0);
+    lv_obj_set_style_bg_color(scr, COLOR_BG, 0);
 
     g_tabview = lv_tabview_create(scr);
     lv_tabview_set_tab_bar_position(g_tabview, LV_DIR_BOTTOM);
@@ -175,7 +165,7 @@ void ui_task_start(void)
 
 void ui_update_system_status(const ui_system_status_t *status)
 {
-    ui_status_page_update(status);
+    ui_status_page_update(status, NULL, 0);
 }
 
 void ui_update_devices(const ui_device_t *devices, int count)
